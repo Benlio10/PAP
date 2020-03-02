a1d1913419d1277b32f8fd2cdfd9f91c49054b35
Benlio10 <leolucaspereiradasilva@gmail.com>
2020-03-02 17:30:58 +0000

    routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,36 @@
 import { Router } from "express";
-import config from "./config/database";
 import EdificioController from "./controllers/EdificioController";
 import SalaController from "./controllers/SalaController";
 import PcController from "./controllers/PcController";
+import AvariaController from "./controllers/AvariaController";
+import UserController from "./controllers/UserController";
 
 const routes = Router();
 
+routes.get("/", (_req, res) => {
+  return res.send("Server running!");
+});
+
+//Routes Edificios
 routes.get("/blocos", EdificioController.index);
 
-routes.get("/salas", SalaController.index);
-routes.post("/salas", SalaController.store);
+//Routes Salas
+routes.get("/blocos/:id_edificio/salas", SalaController.index);
+routes.post("/blocos/:id_edificio/salas", SalaController.store);
 routes.get("/salas/:id", SalaController.show);
 routes.put("/salas/:id", SalaController.update);
 
-routes.get("/pcs", PcController.index);
+//Routes PCs
+routes.get("/salas/:id_sala/pcs", PcController.index);
+routes.post("/salas/:id_sala/pcs", PcController.store);
+
+//Routes Avarias
+routes.get("/avarias", AvariaController.index);
+routes.post("/pcs/:id_pc/avarias", AvariaController.store);
+routes.get("/avarias/:id", AvariaController.show);
+routes.put("/avarias/:id", AvariaController.update);
+
+//Routes Users
+routes.get("/users", UserController.show);
 
 export default routes;
