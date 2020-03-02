e6d2c0c752305d127985e08d8e9abda6239d93c5
Benlio10 <leolucaspereiradasilva@gmail.com>
2020-03-02 17:28:15 +0000

    UserRoutesModels

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,20 @@
+import User from "../models/User";
+import bcrypt from "bcryptjs";
+
+export default {
+  async show(req, res) {
+    const { n_processo, password } = req.query;
+
+    const user = await User.findOne({
+      where: {
+        n_processo
+      }
+    });
+
+    if (bcrypt.compareSync(password, user.password)) {
+      return res.json(user);
+    }
+
+    return res.status(400), res.json({ error: "Wrong password" });
+  }
+};
